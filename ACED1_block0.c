14f21e50c14092785eb11f3ffc8ad66a4afa2d65
Hui Zhou <huizhou07@gmail.com>
2017-05-30 18:18:14 +0200

    Update ACED1_block0.c

--- ACED1_block0.c
+++ ACED1_block0.c
@@ -375,14 +375,14 @@ void ACED_Block(unsigned int x0, unsigned int y0, unsigned int width, unsigned i
                             c_max = c;
                         }
                     }
-		    float p_re = p[0];
+		    /*float p_re = p[0];
 		    unsigned int c_re = 1;
 		    for(c=1; c<9; c++){
 			if(p_re==p[c]){
 			    c_re++;
 			}
 		    }
-		    if(c_re>=8){c_max=rand()%9;}
+		    if(c_re>=8){c_max=rand()%9;}*/
 
 
                     float p_r = (float) rand()/RAND_MAX;
